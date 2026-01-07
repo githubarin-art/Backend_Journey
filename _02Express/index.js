6c52dad77aef5e9638868fc9d1c9634b9a415686
Arin Gupta <aringupta2244@gmail.com>
2026-01-07 23:41:41 +0530

    Add logging functionality with Winston and Morgan; update dependencies

--- _02Express/index.js
+++ _02Express/index.js
@@ -1,9 +1,28 @@
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let tasks = [];
 let taskId = 1;
